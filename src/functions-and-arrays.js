da449bd21cbf1dfe4a102850c065e0e9f358c9b4
vinceevogue <ferreiravincent115@gmail.com>
2025-02-20 14:41:27 +0100

    solve function and array

--- src/functions-and-arrays.js
+++ src/functions-and-arrays.js
@@ -1,34 +1,85 @@
 // Iteration 1 | Find the Maximum
-function maxOfTwoNumbers() {}
-
-
-
+function maxOfTwoNumbers(num1, num2) {
+  if (num1 > num2) {
+    return num1;
+  } else if (num1 < num2) {
+    return num2;
+  } else {
+    return num1, num2;
+  }
+}
 
 // Iteration 2 | Find the Longest Word
-const words = ["mystery", "brother", "aviator", "crocodile", "pearl", "orchard", "crackpot"];
+const words = [
+  "mystery",
+  "brother",
+  "aviator",
+  "crocodile",
+  "pearl",
+  "orchard",
+  "crackpot",
+];
 
-function findLongestWord() {}
-
-
-
+function findLongestWord(array) {
+  if (array.length === 0) {
+    return null;
+  } else {
+    let bigWord = "";
+    array.forEach((element) => {
+      if (element.length > bigWord.length) {
+        bigWord = element;
+      }
+    });
+    return bigWord;
+  }
+}
 
 // Iteration 3 | Sum Numbers
 const numbers = [6, 12, 1, 18, 13, 16, 2, 1, 8, 10];
 
-function sumNumbers() {}
-
-
-
+function sumNumbers(array) {
+  let sum = 0;
+  if (array.length === 0) {
+    return sum;
+  } else {
+    array.forEach((element) => {
+      sum += element;
+    });
+    return sum;
+  }
+}
 
 // Iteration 4 | Numbers Average
 const numbers2 = [2, 6, 9, 10, 7, 4, 1, 9];
 
-function averageNumbers() {}
-
-
-
+function averageNumbers(array) {
+  let average = 0;
+  if (array.length === 0) {
+    return average;
+  } else {
+    array.forEach((element) => {
+      average += element;
+    });
+    return average / array.length;
+  }
+}
 
 // Iteration 5 | Find Elements
-const words2 = ["machine", "subset", "trouble", "starting", "matter", "eating", "truth", "disobedience"];
+const words2 = [
+  "machine",
+  "subset",
+  "trouble",
+  "starting",
+  "matter",
+  "eating",
+  "truth",
+  "disobedience",
+];
 
-function doesWordExist() {}
+function doesWordExist(array, word) {
+  if (array.length === 0) {
+    return null;
+  } else {
+    return array.includes(word);
+  }
+}
